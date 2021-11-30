6e14147a3523102cbcd551a33fd049c17d7c8a5e
Roxanne Weber <marie.sankofa@gmail.com>
2021-11-30 14:25:00 -0600

    added category filter from original menu item list

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ function App() {
 	const [menuItems, setMenuItems] = useState(items);
 	//console.log(menuItems);
 	const [categories, setCatagories] = useState([]);
+	const filterItems = (category) => {
+		const newItems = items.filter((item) => item.category === category);
+		setMenuItems(newItems);
+	};
 
 	return (
 		<main>
@@ -15,7 +19,7 @@ function App() {
 					<h2>My Menu App</h2>
 					<div className='underline'></div>
 				</div>
-
+				<Categories filterItems={filterItems} />
 				<Menu items={menuItems} />
 			</section>
 		</main>
